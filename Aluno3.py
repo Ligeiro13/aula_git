ffc60ea68a6072730adc5cbfc9f7a6a23dd3cf44
Alexandreae <alex.almeida.1510@gmail.com>
2017-03-21 15:07:57 -0300

    modificado aluno 1 e adicionado aluno3

---
+++ Aluno3.py
@@ -0,0 +1,5 @@
+from aluno1 import ler_numeros
+from Aluno2 import maior
+n=int(input("digite um numero: "))
+a=ler_numeros(n)
+print(maior(a))
--- aluno1.py
+++ aluno1.py
@@ -5,7 +5,6 @@ Created on Tue Mar 21 14:46:32 2017
 
 @author: gabrielligeiro
 """
-n = int(input("Digite um numero: "))
 def ler_numeros(n):
     lista = []
     for i in range(n):
